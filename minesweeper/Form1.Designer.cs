3c6b70f8b439e807b583f35ce79536c77fa789ff
Alexey Vetrov <vetaleha@gmail.com>
2015-05-14 15:55:01 +0600

    Created project, implemented design.

---
+++ minesweeper/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace minesweeper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.новичокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.любительToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.пРофессионалToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(340, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаяИграToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.новичокToolStripMenuItem,
+            this.любительToolStripMenuItem,
+            this.пРофессионалToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.toolStripMenuItem2,
+            this.toolStripSeparator3,
+            this.выходToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.gameToolStripMenuItem.Text = "Игра";
+            // 
+            // новаяИграToolStripMenuItem
+            // 
+            this.новаяИграToolStripMenuItem.Name = "новаяИграToolStripMenuItem";
+            this.новаяИграToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.новаяИграToolStripMenuItem.Text = "Новая игра";
+            this.новаяИграToolStripMenuItem.Click += new System.EventHandler(this.новаяИграToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(173, 6);
+            // 
+            // новичокToolStripMenuItem
+            // 
+            this.новичокToolStripMenuItem.Name = "новичокToolStripMenuItem";
+            this.новичокToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.новичокToolStripMenuItem.Text = "Новичок";
+            // 
+            // любительToolStripMenuItem
+            // 
+            this.любительToolStripMenuItem.Name = "любительToolStripMenuItem";
+            this.любительToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.любительToolStripMenuItem.Text = "Любитель";
+            // 
+            // пРофессионалToolStripMenuItem
+            // 
+            this.пРофессионалToolStripMenuItem.Name = "пРофессионалToolStripMenuItem";
+            this.пРофессионалToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.пРофессионалToolStripMenuItem.Text = "Профессионал";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(173, 6);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(176, 22);
+            this.toolStripMenuItem2.Text = "Таблица рекордов";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(173, 6);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 330);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Сапер";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новаяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem новичокToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem любительToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem пРофессионалToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ minesweeper/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Text;
+using System.IO;
+using System.Reflection;
+
+namespace minesweeper
+{
+    public partial class MainForm : Form
+    {
+        static private PrivateFontCollection privFonts = null;
+        public Font digitsFont { get; private set; }
+        private Field field;
+
+        [DllImport("gdi32.dll", ExactSpelling = true)]
+        private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, int cbFont, IntPtr pdv, [In] ref uint pcFonts);
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitFonts()
+        {
+            byte[] fontFile = Properties.Resources.digits;
+
+            IntPtr fontBuff = Marshal.AllocCoTaskMem(fontFile.Length);
+            Marshal.Copy(fontFile, 0, fontBuff, fontFile.Length);
+
+            uint cFonts = 0;
+            AddFontMemResourceEx(fontBuff, fontFile.Length, IntPtr.Zero, ref cFonts);
+
+            privFonts = new PrivateFontCollection();
+            privFonts.AddMemoryFont(fontBuff, fontFile.Length);
+            Marshal.FreeCoTaskMem(fontBuff);
+
+            if (privFonts.Families.Length > 0)
+            {
+                digitsFont = new Font(privFonts.Families[0], 30f, FontStyle.Regular);
+            }
+        }        
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            InitFonts();
+            field = new Field(this);
+        }
+
+        private void новаяИграToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            field.NewGame();
+        }
+    }
+
+    public class FieldButton : Button
+    {
+        public Point pos { get; private set; }
+        public FieldButton(Point pos) : base()
+        {
+            this.pos = pos;
+        }
+    }
+
+    public class Field
+    {
+        private MainForm targetForm;
+        private Label leftMinesLabel;
+        private Label stateLabel;
+        private Label currentTimeLabel;
+        private FieldButton[,] buttonArray;
+        private int[,] origField;
+        private int[,] openedField;
+
+        private int width;
+        private int height;
+        private int numberOfMines;
+        private int leftMines;
+        private int currentTime;
+
+        private void CreateField()
+        {
+            UnbindButtons();
+            origField = new int[height, width];
+            openedField = new int[height, width];
+            buttonArray = new FieldButton[height, width];
+            stateLabel.Text = ":)";
+            leftMines = numberOfMines; 
+            BindButtons();
+        }
+
+        private void BindButtons()
+        {
+            if (buttonArray != null)
+                for (int i = 0; i < height; i++)
+                    for (int j = 0; j < width; j++)
+                    {
+                        buttonArray[i, j] = new FieldButton(new Point(i, j));
+                        targetForm.Controls.Add(buttonArray[i, j]);
+                    }
+        }
+
+        private void UnbindButtons()
+        {
+            if (buttonArray != null)
+                for (int i = 0; i < height; i++)
+                    for (int j = 0; j < width; j++)
+                        if(targetForm.Controls.Contains(buttonArray[i, j]))
+                        {
+                            targetForm.Controls.Remove(buttonArray[i, j]);
+                            buttonArray[i, j].Dispose();
+                        }
+        }
+
+        private void UpdateLayout()
+        {
+            int cellHeight = 20;
+            int cellWidth = 20;
+            targetForm.Width = cellWidth * width + 35;
+            targetForm.Height = cellHeight * height + 140;
+            
+            Point buttonStart = new Point(10, 90);
+            leftMinesLabel.Location = new Point(10, 30);
+            stateLabel.Location = new Point((targetForm.Width - stateLabel.Width) / 2, 30);
+            currentTimeLabel.Location = new Point(targetForm.Width - 10 - currentTimeLabel.Width, 50);
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    buttonArray[i, j].Height = cellHeight;
+                    buttonArray[i, j].Width = cellWidth;
+                    buttonArray[i, j].Location = new Point(buttonStart.X + j * cellWidth, buttonStart.Y + i * cellHeight);
+                }
+            }
+        }
+
+        public void NewGame()
+        {
+            CreateField();
+            UpdateLayout();
+        }
+
+        public Field(MainForm form)
+        {
+            targetForm = form;
+
+            leftMines = 10;
+            leftMinesLabel = new Label();
+            leftMinesLabel.AutoSize = false;
+            leftMinesLabel.Width = 80;
+            leftMinesLabel.Height = 55;
+            leftMinesLabel.Padding = new Padding(0, 0, 0, 0);
+            leftMinesLabel.Text = leftMines.ToString("D3");
+            leftMinesLabel.Font = targetForm.digitsFont;
+            leftMinesLabel.FlatStyle = FlatStyle.Standard;
+            //leftMinesLabel.TextAlign = ContentAlignment.MiddleCenter;
+            leftMinesLabel.BorderStyle = BorderStyle.Fixed3D;
+            leftMinesLabel.BackColor = Color.Black;
+            leftMinesLabel.ForeColor = Color.Red;
+            targetForm.Controls.Add(leftMinesLabel);
+
+            stateLabel = new Label();
+            stateLabel.AutoSize = true;
+            stateLabel.Text = ":)";
+            stateLabel.Font = new Font(FontFamily.GenericSansSerif, 30.0f, FontStyle.Bold);
+            targetForm.Controls.Add(stateLabel);
+
+            currentTime = 0;
+            currentTimeLabel = new Label();
+            currentTimeLabel.Text = currentTime.ToString("D3");
+            currentTimeLabel.Font = targetForm.digitsFont;
+            currentTimeLabel.Height = 50;
+            currentTimeLabel.Width = 70;
+            //targetForm.Controls.Add(currentTimeLabel);
+
+            width = 10;
+            height = 10;
+            numberOfMines = 10;
+            NewGame();
+        }
+    }
+}
